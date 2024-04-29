65aac9c8aa0ea09d247024a9120436403ad28b8a
dhanashrikhadse <pallavikhadse416@gmail.com>
2024-04-29 17:21:47 +0430

    update

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import RandomQuote from './RandomQuote'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RandomQuote />
+  </React.StrictMode>,
+)
